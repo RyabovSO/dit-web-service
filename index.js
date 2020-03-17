4479101c7cc5b34a732f98f88803f22c08f61b08
Рябов Сергей Олегович <RyabovSO@hq.corp.mos.ru>
2020-03-17 10:12:41 +0300

    add test web-service

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// создаем парсер для данных application/x-www-form-urlencoded
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.get('/favicon.ico', (request, response) => {
+    response.send(request.params)
+})
+
+app.get('/:telephone', (request, response) => {
+    response.send(request.params.telephone)
+})
+
+app.post('/', urlencodedParser, (request, response) => {
+    response.send(request.body)
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+})
