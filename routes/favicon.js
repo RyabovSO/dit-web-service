ea10295ea6e39c538473f587cf7fa9f46827758a
unknown <ryabov.ser.o@gmail.com>
2020-03-18 18:57:53 +0300

    refactoring

--- routes/favicon.js
+++ routes/favicon.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* get*/
-router.get('/', function(request, res, next) {
-  	//парсим request id
-    app.get('/favicon.ico', (request, response) => {
+router.get('/favicon.ico', (request, response) => {
     response.send(request.params)
 })
 
