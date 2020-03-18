da7a8bf16787987962b4ecbe7b2dad5c5e9ba7d9
unknown <ryabov.ser.o@gmail.com>
2020-03-18 15:11:52 +0300

    test

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ app.post('/', urlencodedParser, (request, response) => {
     .then(response => response.text())
     .then(resultRequest => {
         resultRequest = JSON.parse(resultRequest);
-        console.log(resultRequest);
+        console.log(resultRequest.notes);
 
         //to 4me notes
-        fetch("https://dit-sd-moscow.4me.qa/v1/notes/request="+requestId, requestOptions4me)
+        fetch("https://dit-sd-moscow.4me.qa/v1/notes/request{id="+requestId+"}", requestOptions4me)
         .then(response => response.text())
         .then(resultNotes => {
             resultNotes = JSON.parse(resultNotes);
