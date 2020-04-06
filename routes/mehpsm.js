d42666219731184e8234ff9545fd351bf0844ed7
unknown <ryabov.ser.o@gmail.com>
2020-04-06 06:39:43 +0300

    add hpsm integration

--- routes/mehpsm.js
+++ routes/mehpsm.js
@@ -29,43 +29,49 @@ router.post('/', urlencodedParser, (request, response) => {
     .then(resultRequest => {
         resultRequest = JSON.parse(resultRequest);
 
-        //to 4me notes
-        fetch("https://dit-sd-moscow.4me.qa/v1/requests/"+requestId+"/notes", requestOptions4me)
-        .then(response => response.text())
-        .then(resultNotes => {
-            resultNotes = JSON.parse(resultNotes);
-            //console.log(resultNotes[0].text);
-            console.log(resultRequest.requested_for);
-            //body
-            var bodyHpsm = {
-                "HPSMInteraction4meREST": {
-                    "User": resultRequest.requested_for.name,
-                    "Description": [resultNotes[0].text],
-                    //"Email": "outout@mail.ru",
-                    "Title":  resultRequest.subject
-                }
-            }
-            //sm header
-			let requestOptionsHPSM = {
-			    method: 'POST',
-			    headers: {
-			        'Content-Type': 'application/json',
-                    'Connection': 'keep-alive',
-			        'Authorization' : 'Basic ' + base64.encode(process.env.USERNAME_HPSM + ":" + process.env.PASSWORD_HPSM)
-			    },
-			    body: JSON.stringify(bodyHpsm),
-			}
-
-            //to hpsm
-            fetch("http://212.11.152.73:13081/SM/9/rest/HPSMInteraction4meREST/", requestOptionsHPSM)
+        //to 4me people
+        fetch("https://dit-sd-moscow.4me.qa/v1/people/"+resultRequest.requested_for.id, requestOptions4me)
+         .then(response => response.text())
+         .then(resultPeople => {
+            //to 4me notes
+            fetch("https://dit-sd-moscow.4me.qa/v1/requests/"+requestId+"/notes", requestOptions4me)
             .then(response => response.text())
-            .then(result => {
-                result = JSON.parse(result);
-                console.log(result.HPSMInteraction4meREST.ID);
+            .then(resultNotes => {
+                resultNotes = JSON.parse(resultNotes);
+                //console.log(resultNotes[0].text);
+                //console.log(resultRequest.requested_for.id);
+                //body
+                var bodyHpsm = {
+                    "HPSMInteraction4meREST": {
+                        //"User": resultRequest.requested_for.name,
+                        "Description": [resultNotes[0].text],
+                        "Email": resultPeople.primary_email,
+                        "Title":  resultRequest.subject
+                    }
+                }
+                //sm header
+		    	let requestOptionsHPSM = {
+		    	    method: 'POST',
+		    	    headers: {
+		    	        'Content-Type': 'application/json',
+                        'Connection': 'keep-alive',
+		    	        'Authorization' : 'Basic ' + base64.encode(process.env.USERNAME_HPSM + ":" + process.env.PASSWORD_HPSM)
+		    	    },
+		    	    body: JSON.stringify(bodyHpsm),
+		    	}
+    
+                //to hpsm
+                fetch("http://212.11.152.73:13081/SM/9/rest/HPSMInteraction4meREST/", requestOptionsHPSM)
+                .then(response => response.text())
+                .then(result => {
+                    result = JSON.parse(result);
+                    console.log(result.HPSMInteraction4meREST.ID);
+                })
+                .catch(error => console.error(error))
             })
             .catch(error => console.error(error))
-            
         })
+         .catch(error => console.error(error))
     })
     .catch(error => console.error(error))
     //
