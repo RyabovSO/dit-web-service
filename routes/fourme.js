c60a67efbd3c373aeb2c17b45d36aca2535844f7
unknown <ryabov.ser.o@gmail.com>
2020-04-11 08:43:15 +0300

    add hpsm integration

--- routes/fourme.js
+++ routes/fourme.js
@@ -8,18 +8,32 @@ const bodyParser = require("body-parser");
 // создаем парсер для данных application/x-www-form-urlencoded
 const urlencodedParser = bodyParser.json();
 
-//4me header
-const requestOptions4me = {
-    method: 'GET',
-    headers: {'Authorization': process.env.AUTHORIZATION_KEY, 'X-4me-Account': 'dit-oiv-it', 'api-token': process.env.TOKIEN_API, 'Content-Type': 'application/x-www-form-urlencoded'},
-    redirect: 'follow'
-}
-
 /* POST*/
 router.post('/', urlencodedParser, (request, response) => {
   	//парсим request id
   	console.log(request.body.id);
     console.log(request.body.custom_fields);
+
+    //4me header
+	let requestOptionsPOST4me = {
+	    method: 'PATCH',
+	    headers: {
+	        'Authorization': process.env.AUTHORIZATION_KEY, 
+	        'X-4me-Account': 'dit-oiv-it', 
+	        'api-token': process.env.TOKIEN_API, 
+	        'Content-Type': 'application/x-www-form-urlencoded'
+	    },
+	    body: JSON.stringify(request.body.custom_fields),
+	}
+
+    fetch("https://dit-sd-moscow.4me.qa/v1/requests/"+request.body.id, requestOptionsPOST4me)
+     .then(response => response.text())
+     .then(result => {
+        result = JSON.parse(result);
+        console.log(result);  
+    })
+
+    .catch(error => console.error(error))
     response.send(request.body);
 });
 
