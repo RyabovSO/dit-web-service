036b4d23254fdb15649279043cdd397be719c2f9
RyabovSO <ryabov.ser.o@gmail.com>
2020-06-10 16:14:12 +0300

    test

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const fourMeToJiraRouter = require('./routes/jira');
-const fourMeToHpsmRouter = require('./routes/hpsm');
-const hpsmToFourMeRouter = require('./routes/fourme');
-const faviconRouter = require('./routes/favicon');
+const test = require('./routes/test');
+//const fourMeToJiraRouter = require('./routes/jira');
+//const fourMeToHpsmRouter = require('./routes/hpsm');
+//const hpsmToFourMeRouter = require('./routes/fourme');
+//const faviconRouter = require('./routes/favicon');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,8 +14,8 @@ app.listen(port, (err) => {
     }
     console.log(`server is listening on ${port}`)
 })
-
-app.use('/fourme', hpsmToFourMeRouter);
-app.use('/hpsm', fourMeToHpsmRouter);
+app.use('/test', test);
+//app.use('/fourme', hpsmToFourMeRouter);
+//app.use('/hpsm', fourMeToHpsmRouter);
 //app.use('/jira', fourMeToHpsmRouter);
 //app.use('/favicon.ico', faviconRouter);
