a5328b4f19ae83cc668f97a33ea6cde82a886308
RyabovSO <ryabov.ser.o@gmail.com>
2020-06-10 16:26:14 +0300

    test

--- routes/test.js
+++ routes/test.js
@@ -5,28 +5,28 @@ const router = express.Router();
 /* POST*/
 router.get('/', (request, response) => {
 
-    response.send(
-	{
-		"id_char":"1",
-		"surname":"Иванов",
-		"name":"Иван",
-		"patronymic":"Иванович",
-		"work_phone":"89778899769",
-		"email":"test1@test.ru",
-		"name_departament":"DIT",
-		"name_position":"ARMA"
-	},
-	{
-		"id_char":"2",
-		"surname":"Петров",
-		"name":"Петр",
-		"patronymic":"Петрович",
-		"work_phone":"89778997655",
-		"email":"test2@test.ru",
-		"name_departament":"DIT",
-		"name_position":"ARMA"
-	}
-)
+    response.send([
+    	{
+			"id_char":"1",
+			"surname":"Иванов",
+			"name":"Иван",
+			"patronymic":"Иванович",
+			"work_phone":"89778899769",
+			"email":"test1@test.ru",
+			"name_departament":"DIT",
+			"name_position":"ARMA"
+		},
+		{
+			"id_char":"2",
+			"surname":"Петров",
+			"name":"Петр",
+			"patronymic":"Петрович",
+			"work_phone":"89778997655",
+			"email":"test2@test.ru",
+			"name_departament":"DIT",
+			"name_position":"ARMA"
+		}
+	])
 });
 
 module.exports = router;
